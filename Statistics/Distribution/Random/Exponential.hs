8e21acb9c7f3f1d4c47fdf66decfa723011295e8
Andres Loeh <mail@andres-loeh.de>
2011-05-15 15:14:27 +0200

    Refactoring Exponential.hs.

--- Statistics/Distribution/Random/Exponential.hs
+++ Statistics/Distribution/Random/Exponential.hs
@@ -4,11 +4,8 @@ module Statistics.Distribution.Random.Exponential (
 
 
 import qualified System.Random.MWC   as R
+import Control.Monad
 import Control.Monad.Primitive (PrimMonad, PrimState)
 
 exponential :: PrimMonad m => R.Gen (PrimState m) -> m Double
-exponential rng = do 
-    u <- R.uniform rng
-    return $ - (log u)
-
-
+exponential rng = liftM (negate . log) (R.uniform rng)
