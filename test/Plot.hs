66b1cffa7a7042bde1eb82e28e45db9a3981f6f9
Andres Loeh <mail@andres-loeh.de>
2011-06-10 21:14:07 +0200

    Moved common plot functions to other module.

--- test/Plot.hs
+++ test/Plot.hs
@@ -4,72 +4,27 @@ module Plot (
 ) where
 
 import Graphics.Rendering.Chart
-import Data.Colour
-import Data.Colour.Names
-import Data.Accessor
 
+import Plot.Utils
 import Statistics.Types
 import Statistics.Distribution
 import qualified Data.Vector.Unboxed as V
-
-(/.) :: (Real a, Real b, Fractional c) => a -> b -> c
-(/.) x y = fromRational $ toRational x / toRational y
 
 plot :: Sample -> String -> FilePath -> IO ()
 plot vs title fn = renderableToSVGFile (toRenderable layout) 800 600 fn
   where
     n = V.length vs
 
-    tv :: [(Double, Double)]
+    tv, ev, dv :: [(Double, Double)]
     tv = [(fromIntegral i, i /. n) | i <- [1 .. n]]
-
-    ev :: [(Double, Double)]
     ev = zip (map fromIntegral [1 .. n]) (V.toList vs)
+    dv = zipWith (\ (x, t) (_, e) -> (x, t - e)) tv ev
 
     layout :: Layout1 Double Double
-    layout = layout1_title      ^= title
-           $ layout1_background ^= solidFillStyle (opaque white)
-           $ layout1_plots      ^= [ Left  (toPlot theoretical),
-                                     Left  (toPlot empirical),
-                                     Right (toPlot difference)]
-           $ setLayout1Foreground (opaque black)
-           $ defaultLayout1
-
-    theoretical :: PlotLines Double Double
-    theoretical = theoreticalWith tv
-
-    empirical :: PlotLines Double Double
-    empirical = empiricalWith ev
-
-    difference :: PlotLines Double Double
-    difference = differenceWith $
-                 zipWith (\ (x, t) (_, e) -> (x, t - e)) tv ev
-
-line_e, line_t, line_d :: CairoLineStyle
-line_e = line_color ^= opaque red   $ line
-line_t = line_color ^= opaque blue  $ line
-line_d = line_color ^= opaque green $ line
-
-line :: CairoLineStyle
-line   = line_width       ^= 1.8
-       $ defaultPlotLines ^. plot_lines_style
-
-lineWith :: CairoLineStyle -> String -> [(Double, a)] ->
-            PlotLines Double a
-lineWith ls txt vs = plot_lines_style ^= ls
-       $ plot_lines_values   ^= [vs]
-       $ plot_lines_title    ^= txt
-       $ defaultPlotLines
-
-theoreticalWith :: [(Double, Double)] -> PlotLines Double Double
-theoreticalWith = lineWith line_t "Theoretical"
-
-empiricalWith :: [(Double, a)] -> PlotLines Double a
-empiricalWith = lineWith line_e "Empirical"
-
-differenceWith :: [(Double, Double)] -> PlotLines Double Double
-differenceWith = lineWith line_d "Difference"
-
+    layout = stdLayout title
+               [ Left  (toPlot (theoreticalWith tv))
+               , Left  (toPlot (  empiricalWith ev))
+               , Right (toPlot ( differenceWith dv)) ]
 
 plotDensity :: (ContDistr d) => d -> Sample -> String -> FilePath -> IO ()
 plotDensity gd vs title fn = renderableToSVGFile (toRenderable layout) 800 600 fn
@@ -81,29 +36,20 @@ plotDensity gd vs title fn = renderableToSVGFile (toRenderable layout) 800 600 f
     bins :: Double
     bins = fromIntegral $ max (n `div` 100) 1000
 
-    xs :: [Double]
+    xs, ys :: [Double]
     xs = map (* (mx / bins)) [0 .. bins]
-
     -- count how many at each level
-    ys :: [Double]
     ys =  let counts = count (V.toList vs) xs
               scale  = fromIntegral n * (mx / bins)
           in  map ((/ scale) . fromIntegral) counts
 
     layout :: Layout1 Double Double
-    layout = layout1_title      ^= title
-           $ layout1_background ^= solidFillStyle (opaque white)
-           $ layout1_plots      ^= [ Left  (toPlot empirical)
-                                   , Left  (toPlot theoretical) ]
-           $ setLayout1Foreground (opaque black)
-           $ defaultLayout1
+    layout = stdLayout title
+               [ Left (toPlot (  empiricalWith (zip xs ys)))
+               , Left (toPlot (theoreticalWith [(x, density gd x) | x <- xs])) ]
 
-    empirical :: PlotLines Double Double
-    empirical = empiricalWith (zip xs ys)
-
-    theoretical :: PlotLines Double Double
-    theoretical = theoreticalWith ([ (x, density gd x) | x <- xs ])
-
+(/.) :: (Real a, Real b, Fractional c) => a -> b -> c
+(/.) x y = fromRational $ toRational x / toRational y
 
 count :: [Double] -> [Double] -> [Int]
 count samples breaks = map length (foldr go (:[]) breaks samples)
